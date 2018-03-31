5c6555a9e9bf7e46228c8843f724939023104258
Ifiok Jr <ifiokotung@gmail.com>
2018-03-31 18:35:29 +0100

    BUILD: setup basic travis build

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+brew update
+brew tap caskroom/cask
+brew cask install unity;
+brew cask install unity-android-support-for-editor
+brew cask install unity-ios-support-for-editor
+brew cask install unity-webgl-support-for-editor
+brew cask install unity-linux-support-for-editor
+brew cask install unity-windows-support-for-editor
+brew cask install unity-android-support-for-editor
+brew cask install unity-standard-assets
